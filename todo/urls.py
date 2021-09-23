ae1cc87b387b997fb8f2719ecadfc623735b61ab
rr2x <79620740+rr2x@users.noreply.github.com>
2021-09-23 18:25:48 +0800

    abstract model, modelforms, view logic

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home44'),
-    path('', views.create_todo, name='create-todo55'),
+    path('create/', views.create_todo, name='create-todo55'),
+    path('todo/<id>/', views.todo_detail, name='todo66'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,37 @@
+from .models import Todo
+from .forms import TodoForm
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
-    return render(request, 'todo/index.html')
+    todos = Todo.objects.all()
+    context = {'todos': todos}
+
+    return render(request, 'todo/index.html', context)
 
 
 def create_todo(request):
-    return render(request, 'todo/create-todo.html')
+    form = TodoForm()
+    context = {'form': form}
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        is_completed = request.POST.get('is_completed', False)
+
+        todo = Todo()
+        todo.title = title
+        todo.description = description
+        todo.is_completed = True if is_completed == "on" else False
+
+        todo.save()
+
+        return HttpResponseRedirect(reverse('todo66', kwargs={'id': todo.pk}))
+
+    return render(request, 'todo/create-todo.html', context)
+
+
+def todo_detail(request, id):
+    return render(request, 'todo/todo-detail.html', {})
