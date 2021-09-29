0b9b4615fb19bc45c4dc34618d7239b57d6007dc
rr2x <79620740+rr2x@users.noreply.github.com>
2021-09-29 12:23:01 +0800

    using postgres, django admin customization

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import User
 
-from django.contrib.auth.admin import UserAdmin
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff')
+    search_fields = ('username', 'email', 'is_staff')
+    list_per_page = 10
+
 
 admin.site.register(User, UserAdmin)
